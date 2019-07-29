3b0bafe7146f453762459c299fd6551cfcc6d7a9
Karapet Hambardzumyan <karapethambardzumyan97@gmail.com>
2019-07-29 11:20:15 +0400

    Maded responsive the game's scene

--- src/index.js
+++ src/index.js
@@ -3,19 +3,21 @@ import Play from './scenes/play';
 
 import { GAME } from './config';
 
-const gameConfig = {
-    type: Phaser.AUTO,
-    width: GAME.width,
-    height: GAME.height,
-    scene: [Play],
-    backgroundColor: '#cccccc',
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 0 },
-            debug: true
+window.onload = () => {
+    const gameConfig = {
+        type: Phaser.AUTO,
+        width: GAME.width,
+        height: GAME.height,
+        scene: [Play],
+        backgroundColor: '#cccccc',
+        physics: {
+            default: 'arcade',
+            arcade: {
+                gravity: { y: 0 },
+                debug: true
+            }
         }
-    }
-};
+    };
 
-new Phaser.Game(gameConfig);
+    new Phaser.Game(gameConfig);
+};
