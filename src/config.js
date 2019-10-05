0cd203ce647ae15bb363677c96cb6cf4f45de2d3
Karapet Hambardzumyan <karapethambardzumyan97@gmail.com>
2019-10-05 19:19:22 +0400

    Updated

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 import RoundRectanglePlugin from '../node_modules/phaser3-rex-notes-master/plugins/roundrectangle-plugin';
 import WebFontLoaderPlugin from '../node_modules/phaser3-rex-notes-master/plugins/webfontloader-plugin';
+import UIPlugin from '../node_modules/phaser3-rex-notes-master/templates/ui/ui-plugin.js';
 import { MAX_WIDTH, MAX_HEIGHT } from './constants';
 
 const width = window.innerWidth > MAX_WIDTH ? MAX_WIDTH : window.innerWidth;
@@ -27,6 +28,13 @@ export default {
                 key: 'WebFontLoader',
                 plugin: WebFontLoaderPlugin,
                 start: true
+            }
+        ],
+        scene: [
+            {
+                key: 'rexUI',
+                plugin: UIPlugin,
+                mapping: 'rexUI'
             }
         ]
     }
